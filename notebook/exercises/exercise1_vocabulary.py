e578392a51c2786bec60e51667afe07be507ab69
Andrea Leone <github@andrealeone.xyz>
2018-04-08 13:08:35 +0200

    0.0.2-.6

--- notebook/exercises/exercise1_vocabulary.py
+++
@@ -1,9 +0,0 @@
-from itertools import product
-
-keyboard = {
-                    "2": "[abc]",  "3": "[def]",
-    "4": "[ghi]",   "5": "[jkl]",  "6": "[mno]",
-    "7": "[pqrs]",  "8": "[tuv]",  "9": "[wxyz]"
-}
-
-combos = [ "".join(i) for i in product(list("23456789"), repeat=4) ]
